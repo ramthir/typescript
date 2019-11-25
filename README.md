7b6387ee645e4db7952034fae58e78b929b7d154
ramthir <ramesh.thiruchelvam@gmail.com>
2019-11-25 08:41:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-ramthi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-ramthi)
---
+++ index.ts
@@ -0,0 +1,26 @@
+// Import stylesheets
+import "./style.css";
+var console = {
+  log: s =>
+    (document.getElementById("app").innerHTML += JSON.stringify(s) + "<br/>")
+};
+
+import { concat, interval } from "rxjs";
+import { map, take } from "rxjs/operators";
+
+const one$ = interval(1000).pipe(
+  take(1),
+  map(() => "1")
+);
+const two$ = interval(1000).pipe(
+  take(2),
+  map(() => "2")
+);
+const three$ = interval(1000).pipe(
+  take(3),
+  map(() => "3")
+);
+
+const result = concat(one$, two$, three$);
+
+result.subscribe(x => console.log(x));
