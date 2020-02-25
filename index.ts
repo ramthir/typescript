b2f4c39ac373295381253655649a69165a0d013b
ramthir <ramesh.thiruchelvam@gmail.com>
2020-02-25 15:46:18 +0530

    timeout & promise.

--- index.ts
+++ index.ts
@@ -1,20 +1,6 @@
 import { console } from './polyfills';
-import { concat, interval } from "rxjs";
-import { map, take } from "rxjs/operators";
 
-const one$ = interval(1000).pipe(
-  take(1),
-  map(() => "1")
-);
-const two$ = interval(1000).pipe(
-  take(2),
-  map(() => "2")
-);
-const three$ = interval(1000).pipe(
-  take(3),
-  map(() => "3")
-);
-
-const result = concat(one$, two$, three$);
-
-result.subscribe(x => console.log(x));
+console.log('🥑 Synchronous 1');
+setTimeout(() => console.log('🥣 Timeout 2'));
+Promise.resolve().then(() => console.log('🍜 Promise 3'));
+console.log('🍍 Synchronous 4');
