3f7d177c26db1123190941fe485332c5926549bf
ramthir <ramesh.thiruchelvam@gmail.com>
2020-06-24 11:41:35 +0530

    fake appointment

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
 import { console } from "./polyfills";
 import { concat, interval, of } from "rxjs";
 import { map, take } from "rxjs/operators";
-import * as faker from 'faker';
+import * as faker from "faker";
 
 const length = 100;
-const intervals = Array.from({ length });
 
-console.log(faker.random.number());
+const appointment = () => ({
+  title: faker.name.firstName(),
+  appendix: faker.name.lastName(),
+  note: faker.lorem.sentence(2)
+});
+
+const intervals = Array.from({ length }).reduce((accumulator, item, index) => {
+  accumulator.push(appointment());
+  return accumulator;
+}, []);
+
+console.log(intervals);
+//console.log(faker.random.number());
