112c52234718e2f5cc477716cadcc32ca935863b
ramthir <ramesh.thiruchelvam@gmail.com>
2020-06-24 11:49:51 +0530

    more data types

--- index.ts
+++ index.ts
@@ -5,6 +5,25 @@ import * as faker from "faker";
 
 const length = 100;
 
+export interface Unit {
+  id: number;
+  name: string;
+}
+
+export interface Room {
+  id: number;
+  name: string;
+  unitId: number;
+}
+
+export interface Appointment {
+  title: string;
+  appendix: string;
+  type: SlotType;
+  status: string;
+  note: string;
+}
+
 enum SlotType {
   AVAILABILITY,
   APPOINTMENT
@@ -16,13 +35,30 @@ enum StatusType {
   ONGOING = "ongoing"
 }
 
-const appointment = () => ({
+const appointment = (): Appointment => ({
   title: faker.name.firstName(),
   appendix: faker.name.lastName(),
   type: SlotType.APPOINTMENT,
   status: faker.random.objectElement(StatusType),
   note: faker.lorem.sentence(2)
 });
+
+export const UNITS: Unit[] = [
+  { id: 1, name: 'Department 1' },
+  { id: 2, name: 'Department 2' },
+  { id: 3, name: 'Department 3' }
+];
+
+export const ROOMS: Room[] = [
+  { id: 1, unitId: 1, name: 'Room 1' },
+  { id: 2, unitId: 1, name: 'Room 2' },
+  { id: 3, unitId: 2, name: 'Room 3' },
+  { id: 4, unitId: 2, name: 'Room 4' },
+  { id: 5, unitId: 2, name: 'Room 5' },
+  { id: 6, unitId: 3, name: 'Room 6' },
+];
+
+
 
 const intervals = Array.from({ length }).reduce((accumulator, item, index) => {
   accumulator.push(appointment());
